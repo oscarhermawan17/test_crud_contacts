f846f8f21816510fb611481f647edb00001eea33
oscarhermawan17 <oscar.hermawan90@gmail.com>
2023-01-31 21:27:27 +0800

    feature: initialize project

---
+++ src/containers/MainPage/MainPage.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Styles = {
+  Text: styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `,
+}  
+
+export { Styles };
+  
---
+++ src/containers/MainPage/index.tsx
@@ -0,0 +1,12 @@
+import { Styles as S } from './MainPage.style'
+
+function MainPage() {
+
+  return (
+    <S.Text>
+      <h1>Test CRUD Contacts</h1>
+    </S.Text>
+  )
+}
+
+export default MainPage
